0bdf6a2c31b9d546c47e077a5d5a1df0d505c961
jdi022222 <jdi022222@gmail.com>
2023-08-31 14:43:47 +0900

    채굴 난이도 설정과 채굴기능 구현

--- src/Block.java
+++ src/Block.java
@@ -6,11 +6,13 @@ public class Block {
 	public String previousHash;
 	private String data;
 	private long timeStamp;
+	private int nonce; //랜덤 생성 토큰
 
 	public Block(String data, String previousHash) {
 		this.data = data;
 		this.previousHash = previousHash;
 		this.timeStamp = new Date().getTime();
+
 		this.hash = calculateHash();
 	}
 
@@ -18,7 +20,17 @@ public class Block {
 		return StringUtil.applySha256(
 			previousHash +
 				timeStamp +
+				nonce +
 				data
 		);
+	}
+
+	public void mineBlock(int difficulty) {
+		String target = new String(new char[difficulty]).replace('\0', '0');
+		while (!hash.substring(0, difficulty).equals(target)) {
+			nonce++;
+			hash = calculateHash();
+		}
+		System.out.println("Block Mined!!! : " + hash);
 	}
 }
